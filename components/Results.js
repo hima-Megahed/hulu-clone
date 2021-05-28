4ce2d2ec08d5c4f0e33be3f27a6fb6f1ea9f2414
Ibrahim Hasan <Ibrahim__Hasan@outlook.com>
2021-05-28 15:44:32 +0200

    adding animation to the main page

--- components/Results.js
+++ components/Results.js
@@ -1,11 +1,12 @@
 import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
 
 export default function Results({ results }) {
   return (
-    <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-items-center">
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-items-center">
       {results.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
-    </div>
+    </FlipMove>
   );
 }
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
+import { forwardRef } from "react";
 
-export default function Thumbnail({ result }) {
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-125 hover:z-50">
+    <div
+      ref={ref}
+      className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-125 hover:z-50"
+    >
       <Image
         layout="responsive"
         width={1920}
@@ -24,4 +28,6 @@ export default function Thumbnail({ result }) {
       </div>
     </div>
   );
-}
+});
+
+export default Thumbnail;
